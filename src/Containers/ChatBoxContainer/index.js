859ade84c1c3c3067e9728eccab34dc8b5eaf460
Sangameswaran A <sangameswaran.a.9355@gmail.com>
2020-08-31 17:09:16 +0530

    Uploaded Images will be stored as encrypted text

--- src/Containers/ChatBoxContainer/index.js
+++ src/Containers/ChatBoxContainer/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import { connect } from 'react-redux';
 import socketIOClient from "socket.io-client";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LZString from 'lz-string';
+import CryptoJS from 'crypto-js';
+import { toast } from "react-toastify";
 import 'styles/chatbox.scss';
 import Request from 'Model/Request';
 import EmojisTab from './EmojisTab.jsx';
@@ -17,9 +20,13 @@ class ChatBoxContainer extends React.PureComponent {
             text: '',
             emojisTabShowFlag: false,
             uploadModalShowFlag: false,
+            downloadModalShowFlag: false,
+            isUploading: false,
         };
+        this.downloadMessage = null;
         this.me = this.props.userInfo;
         this.socket = socketIOClient();
+        this.downloadImg = this.downloadImg.bind(this);
     }
 
     toggleEmojiTabShowFlag = (flag = !this.state.emojisTabShowFlag) => {
@@ -36,6 +43,11 @@ class ChatBoxContainer extends React.PureComponent {
         this.setState({ text: text + emoji });
     }
 
+    setupImgDownload = (message) => {
+        this.downloadMessage = message;
+        this.toggleDownloadModalShowFlag();
+    }
+
     getMessage = (message, key) => {
         const me = this.me || {};
         return <div key={key} className="chatbox-message-box-outer">
@@ -44,7 +56,7 @@ class ChatBoxContainer extends React.PureComponent {
                     <b>{message.from._id == me._id ? 'You' : message.from.name}</b>
                     <span className="chatbox-message-time">{moment(message.createdAt).format('h:mm a')}</span>
                 </div>
-                {message.type == 1 ? <a href={`/uploads/${message.file.filename}`} target="_blank" download={message.file.originalname}>{message.file.originalname}</a> :
+                {message.type == 1 ? <span className="chatbox-message-link" onClick={() => this.setupImgDownload(message)}>{message.file.name}</span> :
                     <span className="chatbox-message-text">{message.text}</span>}
             </div >
         </div>
@@ -76,7 +88,7 @@ class ChatBoxContainer extends React.PureComponent {
         e.preventDefault();
         let text = this.state.text.trim();
         if (text.length == 0) return;
-        Request.post("user/message", { text });
+        Request.post("user/message", { text, type: 0 }).catch(error => toast.error(error.message));
     }
 
     getEmojisTab() {
@@ -113,17 +125,36 @@ class ChatBoxContainer extends React.PureComponent {
         this.setState({ uploadModalShowFlag: !this.state.uploadModalShowFlag });
     }
 
+    toggleDownloadModalShowFlag = () => {
+        this.setState({ downloadModalShowFlag: !this.state.downloadModalShowFlag });
+    }
+
     uploadImg = (e) => {
         e.preventDefault();
+        this.setState({ isUploading: true });
         const file = document.getElementById("upload-img").files[0];
-        const fd = new FormData();
-        fd.append('upload-img', file);
-        Request.post('user/upload', fd, { headers: { 'Content-Type': undefined } }).then(resp => this.toggleUploadModalShowFlag());
+        const reader = new FileReader();
+        reader.addEventListener('load', (event) => {
+            const text = event.target.result;
+            const etext = CryptoJS.AES.encrypt(text, $("#img-upload-password").val()).toString();
+            const content = LZString.compressToUTF16(etext);
+            Request.post("user/message", { type: 1, file: { content, name: file.name } }).then(resp => {
+                this.setState({ isUploading: false });
+                this.toggleUploadModalShowFlag();
+            }).catch(error => {
+                this.setState({ isUploading: false });
+                toast.error(error.message)
+            });
+        });
+        reader.readAsDataURL(file);
     }
 
     getUploadModalBody() {
         return <form onSubmit={this.uploadImg}>
-            <div><input id="upload-img" type="file" accept="image/*" /></div>
+            <div><input id="upload-img" type="file" accept="image/*" required /></div>
+            <label htmlFor="img-upload-password">Password</label>
+            <div><input type="password" id="img-upload-password" required /></div>
+            {this.state.isUploading && <div>Uploading please wait...</div>}
             <div className="d-flex">
                 <button className="sg-btn m-auto" type="submit">Upload</button>
             </div>
@@ -138,9 +169,55 @@ class ChatBoxContainer extends React.PureComponent {
             onClose={this.toggleUploadModalShowFlag} />
     }
 
+    async downloadImg(e) {
+        e.preventDefault();
+        let { content } = this.downloadMessage.file;
+        if (!content) {
+            const { _id } = this.downloadMessage;
+            try {
+                const resp = await Request.get(`user/message/${this.downloadMessage._id}`);
+                content = this.downloadMessage.file.content = LZString.decompressFromUTF16(resp.file.content);
+            } catch (e) {
+                return toast.error(e.message);
+            }
+        }
+        let text;
+        try {
+            text = CryptoJS.AES.decrypt(content, $("#img-download-password").val()).toString(CryptoJS.enc.Utf8);
+        } catch (e) {
+            return toast.error("Invalid password");
+        }
+        var element = document.createElement('a');
+        element.setAttribute('href', text);
+        element.setAttribute('download', this.downloadMessage.file.name);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    getDownloadModalBody() {
+        return <form onSubmit={this.downloadImg}>
+            <label htmlFor="img-download-password">Password</label>
+            <div><input type="password" id="img-download-password" required autoFocus /></div>
+            <div className="d-flex">
+                <button className="sg-btn m-auto" type="submit">Download</button>
+            </div>
+        </form>;
+    }
+
+    getDownloadModal() {
+        return <AbstractModal
+            header="Download"
+            body={this.getDownloadModalBody()}
+            show={this.state.downloadModalShowFlag}
+            onClose={this.toggleDownloadModalShowFlag} />
+    }
+
     render() {
         return <div className={`chatbox`}>
             {this.getUploadModal()}
+            {this.getDownloadModal()}
             <div className="chatbox-header sg-bg-primary">
                 <span className="chatbox-title">Messages</span>
                 <button className="sg-btn" onClick={this.logout}>Logout</button>
@@ -168,7 +245,7 @@ class ChatBoxContainer extends React.PureComponent {
     componentDidMount() {
         this.scrollToBottom();
         this.fetchMessages();
-        this.socket.on('NEW_MESSAGE', this.pushMessage);
+        this.socket.on('EC_NEW_MESSAGE', this.pushMessage);
     }
 }
 
